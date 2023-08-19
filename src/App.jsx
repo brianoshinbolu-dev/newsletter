88f1834616f40521fe0c2c1c5b0d21c826b7272e
Brian <oshinbolubrian@gmai;.com>
2023-08-19 19:46:45 +0100

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import './App.css'
+import "./App.css";
 import Newletter from "./Pages/Newletter.jsx";
 import ThankYou from "./Pages/ThankYou.jsx";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { EmailContext } from "./Context";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Newletter />} />
-        <Route path="/thankyou" element={<ThankYou />} />
-      </Routes>
-    </Router>
-  )
+    <EmailContext.Provider value={{ data, setData }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Newletter />} />
+          <Route path="/thankyou" element={<ThankYou />} />
+        </Routes>
+      </Router>
+    </EmailContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/Pages/ThankYou.jsx
+++ src/Pages/ThankYou.jsx
@@ -1,22 +1,27 @@
-import "./thankyou.css"
+import "./thankyou.css";
 import icon from "../assets/images/icon-list.svg";
-import {useNavigate} from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import { EmailContext } from "../Context";
+import { useContext } from "react";
 
 function ThankYou() {
+  const { data } = useContext(EmailContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-      <div className="tk-container">
+    <div className="tk-container">
       <div>
-        <img src={icon} className="tk-icon" alt="list-icon"/>
+        <img src={icon} className="tk-icon" alt="list-icon" />
         <h1>Thanks for subscribing!</h1>
-        <p>A confirmation email has been sent to your email. Please open it and click the button inside to confirm your subscription.</p>
-        </div>
-        <button onClick={() => navigate("/")}>Dismiss message</button>
+        <p>
+          A confirmation email has been sent to <strong>{data}</strong>. Please
+          open it and click the button inside to confirm your subscription.
+        </p>
       </div>
-  )
+      <button onClick={() => navigate("/")}>Dismiss message</button>
+    </div>
+  );
 }
 
-export default ThankYou
+export default ThankYou;
